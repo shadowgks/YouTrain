11004a25666e3017f2a86db6397d5f1634437eb1
fouad <fouadwork2@gmail.com>
2022-12-15 14:37:57 +0100

    fix the insert

--- includes/scripts.php
+++ includes/scripts.php
@@ -7,8 +7,14 @@ $train_object = new Train();
 if (isset($_POST['save']))   $train_object->insert($_POST);
 if (isset($_POST['update']))   $train_object->update($_POST);
 if (isset($_POST['delete']))  $train_object->delete($_POST);
-//end crud fouad
 
+include(__DIR__ . '/../classes/reservationClass.php');
+$reservation_object = new Reservation;
+if (isset($_POST['book-now'])) {
+    $reservation_object->insertReservetion($_POST, $_SESSION);
+    echo "<script>window.location.replace('../voyages.php')</script>";
+}
+//end crud fouad
 // ====================================================
 // Begin saad
 include __DIR__ . '/../classes/voyagesClass.php';
